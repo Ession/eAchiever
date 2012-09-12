2249b91e5bf06a11f828d52cfc1e6f68566165b8
dark <mail@mathiasjost.com>
2012-09-13 01:31:44 +0200

    initial commit
    initial commit of the addon files.

---
+++ eAchiever.lua
@@ -0,0 +1,48 @@
+-- =============================================================================
+-- 
+--       Filename:  eAchiever.lua
+-- 
+--    Description:  Takes a screenshot when you earn a achievement.
+-- 
+--        Version:  5.0.1
+-- 
+--         Author:  Mathias Jost (mail@mathiasjost.com)
+-- 
+-- =============================================================================
+
+
+-- -----------------------------------------------------------------------------
+-- Create the main frame and other variables for the addon
+-- -----------------------------------------------------------------------------
+local eAchiever = CreateFrame("Frame")
+local timer     = 0
+local threshold = 1.2
+
+-- -----------------------------------------------------------------------------
+-- Registers the events
+-- -----------------------------------------------------------------------------
+eAchiever:RegisterEvent("ACHIEVEMENT_EARNED")
+
+
+-- -----------------------------------------------------------------------------
+-- Eventhandler
+-- -----------------------------------------------------------------------------
+eAchiever:SetScript("OnEvent", function()
+	
+	-- Set OnUpdate for timer on earning an achievement
+	eAchiever:SetScript("OnUpdate", function(self, elapsed)
+	
+		-- add the time since the last update occured to the timer
+		timer = timer + elapsed
+		
+		-- if the timer reached it's threshold take a screenshot, 
+		-- reset timer and unregister the OnUpdate function
+		if timer >= threshold then
+			Screenshot()
+			timer = 0
+			eAchiever:SetScript("OnUpdate", nil)
+		end
+		
+	end)
+	
+end) -- eAchiever:SetScript("OnEvent", function()
